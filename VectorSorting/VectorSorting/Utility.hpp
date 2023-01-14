a1ffdc23f7faab919b785ad6b2eb5c89db2c4464
Joshua Wozny <joshua.wozny@snhu.edu>
2023-01-13 21:28:47 -0500

    Add VectorSorting back to project

---
+++ VectorSorting/VectorSorting/Utility.hpp
@@ -0,0 +1,21 @@
+#ifndef     _UTILITY_HPP_
+# define    _UTILITY_HPP_
+
+#include <string>
+
+using std::string;
+
+/**
+ * Simple C function to convert a string to a double
+ * after stripping out unwanted char
+ *
+ * credit: http://stackoverflow.com/a/24875936
+ *
+ * @param ch The character to strip out
+ */
+double strToDouble(string str, char ch) {
+    str.erase(remove(str.begin(), str.end(), ch), str.end());
+    return atof(str.c_str());
+}
+
+#endif /*!_UTILITY_HPP_*/
