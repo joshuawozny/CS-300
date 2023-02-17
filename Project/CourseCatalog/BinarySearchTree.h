861c50c7f30aff52eaf2be687f0ca8d46c6ef4ab
Joshua Wozny <joshua.wozny@snhu.edu>
2023-02-17 08:44:40 -0500

    Update BinarySearchTree.h

--- Project/CourseCatalog/BinarySearchTree.h
+++ Project/CourseCatalog/BinarySearchTree.h
@@ -4,12 +4,13 @@
 // Author      : Joshua Wozny
 // Version     : 1.0
 // Last Updated: January 30, 2023
-// Copyright   : Copyright © 2023,  portions Copyright © 2017 SNHU COCE
+// Copyright   : Copyright Â© 2023,  portions Copyright Â© 2017 SNHU COCE
 // Description : CS-300 Project Two, BinarySearchTree object
 //============================================================================
 
 #include "Course.h"
 
+//Defines a Node object
 struct Node
 {
     // member vars
@@ -22,6 +23,7 @@ struct Node
     Node(Course course) : Node() {
         this->course = course;
     }
+    //Destructor deletes right and left node pointers, which will recusively delete all node pointers from the tree
     ~Node() {
         delete left;
         delete right;
@@ -30,16 +32,19 @@ struct Node
 
 class BinarySearchTree
 {
+    // private variables and methods used by the class
     Node* root;
 
     void printInorder(struct Node* node);
     Node* addNode(Node* node, Course course);
 
 public:
+    //Constructors
     BinarySearchTree();
     BinarySearchTree(Node* rootNode);
+    //Destructor
     virtual ~BinarySearchTree();
-
+    //Public methods
     void Insert(Course course);
     void PrintCourseList();
     void PrintCourse(string courseNumber);
