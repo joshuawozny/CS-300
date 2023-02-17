2bc2050d3b7b66afd6270c9333c2e78558b26591
Joshua Wozny <joshua.wozny@snhu.edu>
2023-02-17 08:50:51 -0500

    Update Course.cpp

--- Project/CourseCatalog/Course.cpp
+++ Project/CourseCatalog/Course.cpp
@@ -1,34 +1,35 @@
 #include "Course.h"
 
+//Default constructor, all variables are already intialized upon creation
 Course::Course(){}
-
+//Contructor with all course members are passed a value
 Course::Course(string courseNumber, string courseName, vector<string> coursePrerequisites) : Course()
 {
 	this->courseNumber = courseNumber;
 	this->courseName = courseName;
 	this->coursePrerequisites = coursePrerequisites;
 }
-
+//Destructor, although vector handles its own garbage collection this will release memory more quickly (potentially) 
+//than waiting for garbage collection to occur
 Course::~Course()
 {
 	vector<string>().swap(this->coursePrerequisites);
 }
-
+//Getters
 string Course::Number()
 {
 	return this->courseNumber;
 }
-
 string Course::Name()
 {
 	return this->courseName;
 }
-
 string Course::Description()
 {
 	return this->courseNumber + ", " + this->courseName;
 }
-
+//Method to display pre-requisites by looping through the prerequisites vector
+//with logic to include a comma except for the last prerequisite
 void Course::DisplayCoursePreprequisites()
 {
 	int preRequisitieCount = coursePrerequisites.size();
